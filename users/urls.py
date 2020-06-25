758b489725dec6a88fb15182ffd6e9c55c34db02
GilbertTan19 <Gilberttan95.gt@gmail.com>
2020-06-25 13:50:46 +0800

    Profile

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProfileUpdateView, TelegramOTPView, ProfileView
+from .views import ProfileUpdateView, TelegramOTPView, ProfileFollowingView, ProfileDiscussionView, ProfileCommentView, ProfileReviewView
 from allauth.account.views import PasswordChangeView
 # from .views import SocialLoginView
 
@@ -7,7 +7,10 @@ urlpatterns = [
     path('update/', ProfileUpdateView.as_view(), name='update_profile'),
     path('telegram_otp/', TelegramOTPView.as_view(), name='telegram_otp'),
     path('password/', PasswordChangeView.as_view(), name='change_password'),
-    path('profile/<slug:username>/', ProfileView.as_view(), name='profile_view'),
+    path('profile/<slug:username>/following/', ProfileFollowingView.as_view(), name='profile_following'),
+    path('profile/<slug:username>/comment/', ProfileCommentView.as_view(), name='profile_comment'),
+    path('profile/<slug:username>/discussion/', ProfileDiscussionView.as_view(), name='profile_discussion'),
+    path('profile/<slug:username>/review/', ProfileReviewView.as_view(), name='profile_review'),
 
 
     # path('google/login/', SocialLoginView.as_view(), name='google_login'),
