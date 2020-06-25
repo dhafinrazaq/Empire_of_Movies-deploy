fe7da3758669539a29c8e146e2c562ec733d7612
dhafin <dhafinrazaq@gmail.com>
2020-06-25 10:28:07 +0700

    created profile view

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProfileUpdateView, TelegramOTPView
+from .views import ProfileUpdateView, TelegramOTPView, ProfileView
 from allauth.account.views import PasswordChangeView
 # from .views import SocialLoginView
 
@@ -7,5 +7,8 @@ urlpatterns = [
     path('update/', ProfileUpdateView.as_view(), name='update_profile'),
     path('telegram_otp/', TelegramOTPView.as_view(), name='telegram_otp'),
     path('password/', PasswordChangeView.as_view(), name='change_password'),
+    path('profile/<slug:username>/', ProfileView.as_view(), name='profile_view'),
+
+
     # path('google/login/', SocialLoginView.as_view(), name='google_login'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,10 +3,11 @@ from django.views.generic import TemplateView
 from django.views.generic import UpdateView, View
 from .forms import CustomUserCreationForm
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 import hashlib
 import datetime
 from .forms import CustomUserChangeForm
+from articles.models import Discussion, Review, Comment
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = get_user_model()
@@ -32,6 +33,21 @@ class TelegramOTPView(LoginRequiredMixin, TemplateView):
         self.request.user.OTP = int(hashlib.sha256((current_date_string + self.request.user.username).encode('utf-8')).hexdigest(), 16) % 10**8
         self.request.user.save()
         context['OTP'] = self.request.user.OTP
+        return context
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'account/profile_view.html'
+    login_url = 'account_login'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print("kwargs======" + str(self.kwargs))
+        User = get_user_model()
+        context['user'] = User.objects.get(username=self.kwargs.get('username'))
+        context['discussions'] = Discussion.objects.filter(author=context['user'])
+        context['reviews'] = Review.objects.filter(author=context['user'])
+        context['comments'] = Comment.objects.filter(author=context['user'])
+        context['following'] = context['user'].follows
         return context
 
 # class SocialLoginView(View):
