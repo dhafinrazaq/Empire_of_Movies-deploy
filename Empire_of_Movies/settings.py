157a5821f53357f18b7f7cb13f579f626321d208
GilbertTan19 <Gilberttan95.gt@gmail.com>
2020-06-28 10:46:25 +0800

    before pull

--- Empire_of_Movies/settings.py
+++ Empire_of_Movies/settings.py
@@ -93,24 +93,24 @@ DATABASES = {
     #     'PORT': '5432',
     # }
 
-    #     'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'empire2',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'gilbert19',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
-    # }
-    
-
-    'default': {
+        'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'empire_of_movies',
-        'USER': 'dhafinrazaq',
-        'PASSWORD': '123',
+        'NAME': 'empire2',
+        'USER': 'postgres',
+        'PASSWORD': 'gilbert19',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
+    
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'empire_of_movies',
+    #     'USER': 'dhafinrazaq',
+    #     'PASSWORD': '123',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
 
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
