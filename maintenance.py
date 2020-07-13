054e825d398ba8655f9a6c8807608dfd4e712846
dhafin <dhafinrazaq@gmail.com>
2020-07-13 12:55:48 +0700

    add on server environment for maintenance

--- maintenance.py
+++ maintenance.py
@@ -3,6 +3,10 @@ django.setup()
 
 # export DJANGO_SETTINGS_MODULE=Empire_of_Movies.settings
 
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Empire_of_Movies.settings'
+
 from articles.models import Movie
 movie_list = Movie.objects.all()
 for movie in movie_list:
